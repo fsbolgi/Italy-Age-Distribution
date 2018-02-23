67c3b0a8b2aa11c144b246f4770667195cee2449
fsbolgi <francesca.sbolgi@gmail.com>
2018-02-23 12:52:17 +0100

    refresh on resize

--- histogram.js
+++ histogram.js
@@ -2,39 +2,51 @@ var age = [20, 40, 60, 80, 100, 120];
 var nPeopleA = [8, 18, 23, 12, 5, 2];
 var nPeopleB = [6, 17, 22, 14, 8, 5];
 
-d3.tsv("2011.tsv", type, function(error, data) {
-    x.domain([0, d3.max(data, function(d) { return d.value; })]);
+window.onresize = function(){ location.reload(); } //refresh script on resize
+
+function screen_width() {
+    if (window.outerWidth < 640)
+        return 350;
+    else
+        return 500;
+}
+
+///////// TODO read data from csv
 
 var xScale = d3.scaleLinear()
     .domain([0, 23])
-    .range([0, 210])
+    .range([0, screen_width()/2-30])
 
-var barA = d3.select(".svg_histoA")
+d3.select(".svg_histoA")
     .selectAll("rect")
     .data(nPeopleA)
-    .enter();
+        .enter()
+            .append("rect")
+            .attr("width", function(d) { return xScale(d); })
+            .attr("height", (screen_width()-50)/nPeopleA.length)
+            .attr("x", function(d){ return (screen_width()/2-(xScale(d)+20)); })
+            .attr("y", function(d, i) { return i * (screen_width())/nPeopleA.length; })
+            .style("stroke", "black")
+            .style("stroke-width", "1px")
+            .style("opacity", .25);
 
-barA.append("rect")
-    .attr("width", function(d) { return xScale(d); })
-    .attr("height", 50)
-    .attr("x", function(d){ return (220-xScale(d)); })
-    .attr("y", function(d, i) { return i * 50; })
-    .style("stroke", "black")
-    .style("stroke-width", "1px")
-    .style("opacity", .25);
+d3.select(".svg_histoB") // where we want to create the histogram
+    .selectAll("rect") // select all rectangles (even if not present)
+    .data(nPeopleB) // you pass the array, everything later will be executed for each element
+        .enter() // what do do if rect is missing
+            .append("rect") // insert new rectangle
+            .attr("width", function(d) { return xScale(d); }) //width of the rect
+            .attr("height", (screen_width()-50)/nPeopleB.length) //height of the rect
+            .attr("x", 20) // position on x coord
+            .attr("y", function(d, i) { return i * (screen_width())/nPeopleB.length; }) // position on y coord
+                        // this function takes as first input the data element and as second the index
+            .style("stroke", "black")
+            .style("stroke-width", "1px")
+            .style("opacity", .25);
 
-barA.append("text")
-    .data(age)
-
-d3.select(".svg_histoB")
-    .selectAll("rect")
-    .data(nPeopleB)
-    .enter()
-    .append("rect")
-    .attr("width", function(d) { return xScale(d); })
-    .attr("height", 50)
-    .attr("x", 30)
-    .attr("y", function(d, i) { return i * 50; })
-    .style("stroke", "black")
-    .style("stroke-width", "1px")
-    .style("opacity", .25);
+function myFunction() {
+    var w = window.outerWidth;
+    var h = window.outerHeight;
+    var txt = "Window size: width=" + w + ", height=" + h;
+    document.write(txt);
+}
