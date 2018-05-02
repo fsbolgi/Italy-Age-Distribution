19ed4bbbb732f34debc41dd4b8a49b41f395949d
fsbolgi <francesca.sbolgi@gmail.com>
2018-05-02 20:08:49 +0200

    zoom to bounding box and better graphics

--- map_img.js
+++ map_img.js
@@ -1,48 +1,64 @@
-function screen_width() {
-    if (window.innerWidth < 640)
-        //return 350;
-        return 500;
-    else
-        return 500;
-}
-
+var width = 500; // width and height of the map
 var svg_map = d3.select(".svg_map_img"); // select correct svg
 
-d3.json("Maps/regTopo.json", function(error, reg) {
-    // read topojson file and calls a function
-    // that takes as input the possible error and the file json itself
+var projection_ita = d3.geo.albers() //projects spherical coordinate to a plane
+    .center([0, 41])
+    .rotate([347, 0])
+    .parallels([35, 45])
+    .scale(width*5.3)
+    .translate([width / 2, width / 1.71]);
+
+var path = d3.geo.path() //path takes the projection and formats it appropriately
+    .projection(projection_ita);
+
+d3.json("Maps/regTopo.json", create_map); // read topojson file and calls a function
+
+function create_map (error, json_reg) { // the input are the possible error and the file json itself
     if (error) return console.error(error); // if file not found print error on console
     //console.log(reg);
     //convert from topo to geo for display
-    var subunits = topojson.feature(reg, reg.objects.regions);
-
-    //projects spherical coordinate to a plane
-    var projection = d3.geo.albers()
-        .center([0, 41])
-        .rotate([347, 0])
-        .parallels([35, 45])
-        .scale(screen_width()*5.4)
-        .translate([screen_width() / 2, screen_width() / 1.71]);
-
-    //path takes the projection and formats it appropriately
-    var path = d3.geo.path()
-        .projection(projection);
+    var regions = topojson.feature(json_reg, json_reg.objects.regions);
 
     // insert the map in the svg
-    svg_map.selectAll(".subunit")
-        .data(subunits.features)
+    svg_map.selectAll(".regions")
+        .data(regions.features)
+        // what to do on opening
         .enter()
-        .append("path")
-        .attr("class", function(d) {return "subunit " + d.id; })
-        .attr("d", path)
-        .on('mouseover', function(d){
+            .append("path")
+            .attr("class", function(d) {return "regions " + d.id; })
+            .attr("d", path)
+            // what to do when a hover over a region
+        .on("mouseover", function(curr_region){
             svg_map.append("text")
-                .text(d.properties.NOME_REG)
-                .attr("transform", "translate(" + path.centroid(d)[0]+", " + (path.bounds(d)[0][1])+ ")")
+                .text(curr_region.properties.NOME_REG)
+                .attr("transform", "translate(" + path.centroid(curr_region)[0]+", " + (path.bounds(curr_region)[0][1]-5)+ ")")
                 .attr("dy", ".35em")
-                .attr("class", "subunit-label " + d.properties.NOME_REG);
+                .attr("class", "regions-label " + curr_region.properties.NOME_REG);
         })
-        .on('mouseout', function(d){
-            d3.selectAll("text").remove();
+        // what to do when hovering out of a region
+        .on("mouseout", function(){ d3.selectAll("text").remove(); })
+        // what to do when clicking on a region
+        .on("click", region_clicked);
+}
+
+function region_clicked(curr_region) {
+    var bounding_box = path.bounds(curr_region);
+    var box_width = bounding_box[1][0] - bounding_box[0][0],
+        box_height = bounding_box[1][1] - bounding_box[0][1],
+        box_center_x = (bounding_box[0][0] + bounding_box[1][0]) / 2,
+        box_center_y = (bounding_box[0][1] + bounding_box[1][1]) / 2;
+    var scale = .9 / Math.max(box_width / width, box_height / width),
+        translate = [width / 2 - scale * box_center_x, width / 2 - scale * box_center_y];
+    d3.select(this).attr("class", "current_region");
+    svg_map.selectAll(".regions").transition()
+        .duration(900)
+        .attr("transform", "translate(" + [width / 2, width / 1.71] + ")scale(0)");
+    d3.select(this).transition()
+        .duration(900)//.ease(d)
+        .style("stroke-width", 1.5 / scale + "px")
+        .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
+    d3.selectAll("text").remove();
+    d3.selectAll(".current_region")
+        .on("mouseover", function (d) {
         });
-});
+}
