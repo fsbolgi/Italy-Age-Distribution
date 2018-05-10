529606ba162b1b6e971017fd91761c0624199691
fsbolgi <francesca.sbolgi@gmail.com>
2018-05-10 15:27:27 +0200

    read data of italy

--- histogram.js
+++ histogram.js
@@ -3,56 +3,80 @@ var nPeopleA = [8, 18, 30, 23, 12, 5, 2];
 var nPeopleB = [6, 17, 22, 32, 14, 8, 5];
 var width = 600;
 var height = 550;
+var svg_histoA = d3.select(".histoA_svg");
+var svg_histoB = d3.select(".histoB_svg");
 
-///////// TODO read data from csv
+d3.csv("Data/Male/1982-ITALIA.csv", type, function (error, data) {
+    console.log(data);
+    var max = d3.max(data, function (d) {
+        return d.value;
+    });
+    var xScale = d3.scale.linear()
+        .domain([0, max])
+        .range([0, width/2-30]);
 
-/*d3.csv("../sample.csv", function(data) {
-    var d = data[0];
-    console.log(d.object());
-});*/
+    var bar_height = (height - 50) / data.length;
 
-function getMaxOfArray(numArray) {
-    return Math.max.apply(null, numArray);
-}
-
-var xScale = d3.scale.linear()
-    .domain([0, getMaxOfArray(nPeopleB)])
-    .range([0, width/2-30])
-
-d3.select(".histoA_svg")
-    .selectAll("rect")
-    .data(nPeopleA)
+    svg_histoA.selectAll("rect")
+        .data(data)
         .enter()
-            .append("rect")
-            .attr("width", function(d) { return xScale(d); })
-            .attr("height", (height-50)/nPeopleA.length)
-            .attr("x", function(d){ return (width/2-(xScale(d)+20)); })
-            .attr("y", function(d, i) { return i * (height)/nPeopleA.length; })
-            .style("stroke", "black")
-            .style("opacity", .5)
-        .on('mouseover', function(d){
-            d3.select(this).style("opacity", .9);
+        .append("rect")
+        .attr("width", function (d) {
+            return xScale(d.value);
         })
-        .on('mouseout', function(d){
-            d3.select(this).style("opacity", .5);
+        .attr("height", bar_height)
+        .attr("x", function (d) {
+            return (width / 2 - (xScale(d.value) + 20));
+        })
+        .attr("y", function (d, i) {
+            return i * (height) / data.length;
+        })
+        .on('mouseover', function (d) {
+            d3.select(this).style("opacity", 1);
+            svg_histoA.append("text")
+                .text("value: " + d.value)
+                .attr("dx", this.x.animVal.value + 35)
+                .attr("dy", this.y.animVal.value + 15)
+                .attr("class", "histo_label");
+        })
+        .on('mouseout', function (d) {
+            d3.select(this).style("opacity", .6);
+            svg_histoA.selectAll("text").remove();
+        })
+        .on("click", function (d) {
+            console.log(d.value);
         });
 
-d3.select(".histoB_svg") // where we want to create the histogram
-    .selectAll("rect") // select all rectangles (even if not present)
-    .data(nPeopleB) // you pass the array, everything later will be executed for each element
-        .enter() // what do do if rect is missing
-            .append("rect") // insert new rectangle
-            .attr("width", function(d) { return xScale(d); }) //width of the rect
-            .attr("height", (height-50)/nPeopleB.length) //height of the rect
-            .attr("x", 20) // position on x coord
-            .attr("y", function(d, i) { return i * (height)/nPeopleB.length; }) // position on y coord
-                        // this function takes as first input the data element and as second the index
-            .style("stroke", "black")
-            .style("opacity", .5)
-        .on('mouseover', function(d){
-            d3.select(this).style("opacity", .9);
+    svg_histoB.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("width", function (d) {
+            return xScale(d.value);
         })
-        .on('mouseout', function(d){
-            d3.select(this).style("opacity", .5);
+        .attr("height", bar_height)
+        .attr("x", 20)
+        .attr("y", function (d, i) {
+            return i * (height) / data.length;
+        })
+        .on('mouseover', function (d) {
+            d3.select(this).style("opacity", 1);
+            svg_histoB.append("text")
+                .text("value: " + d.value)
+                .attr("dx", this.x.animVal.value + 35)
+                .attr("dy", this.y.animVal.value + 15)
+                .attr("class", "histo_label");
+        })
+        .on('mouseout', function (d) {
+            d3.select(this).style("opacity", .6);
+            svg_histoB.selectAll("text").remove();
+        })
+        .on("click", function (d) {
+            console.log(d.value);
         });
+});
 
+function type(d) {
+    d.value = +d.IT;
+    return d;
+}
