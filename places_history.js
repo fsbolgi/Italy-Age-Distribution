a2fed51430cc0c7fb8741b6ba4df787bfcc263d4
fsbolgi <francesca.sbolgi@gmail.com>
2018-05-15 18:42:45 +0200

    correct remove when zoom out

--- places_history.js
+++ places_history.js
@@ -1,5 +1,5 @@
 var svg_places = d3.select(".places_history_svg"), // select correct svg
-    x = 100,
+    x = 90,
     y = 10;
 var grp = svg_places.append("g")
     .attr("transform", "translate(" + x + "," + y + ")");
@@ -8,36 +8,38 @@ grp.append("text")
     .text("ITALIA")
     .attr("text-anchor", "middle")
     .attr("dy", ".35em")
-    .attr("class", "places_text");
+    .attr("class", "places_text_IT");
 
-function draw_arrow(el_name) {
+function draw_arrow(el_name, level) {
     var line = svg_places.append("line") // draw line
         .attr("x1", x)
-        .attr("y1", y+25)
+        .attr("y1", (y + 25 + 180 * level))
         .attr("x2", x)
-        .attr("y2", y+150)
+        .attr("y2", (y + 150 + 180 * level))
         .attr("stroke-width", 0)
         .attr("stroke", "#696630")
         .transition()
         .duration(700)
         .attr("stroke-width", 1.7)
-        .attr("marker-end", "url(#arrow)");
+        .attr("marker-end", "url(#arrow)")
+        .attr("class", "arrow "+ level);
 
-    y = y + 180;
-
-    var grp = svg_places.append("g")
-        .attr("transform", "translate(" + x + "," + y + ")");
+    grp = svg_places.append("g")
+        .attr("transform", "translate(" + x + "," + (y + 180 * (level + 1)) + ")");
 
     var min_font = 0;
+
+    svg_places.selectAll("text").on("click", function () { // make previous levels
+    });
 
     var place_name = grp.append("text")
         .text(extract_properties(el_name)[0].toUpperCase())
         .attr("text-anchor", "middle")
         .attr("dy", ".35em")
-        .attr("class", "places_text")
+        .attr("class", "places_text "+level)
         .style("font-size", 1)
         .on("click", function () {
-            d3.select(this).style("font-weight", "bold");
+            zoom_out();
         });
     place_name.transition()
         .duration(700)
@@ -45,4 +47,9 @@ function draw_arrow(el_name) {
             min_font = Math.min(16, (200) / this.getComputedTextLength() * 0.55);
             return min_font + "pt";
         });
+}
+
+function remove_level() {
+    svg_places.selectAll(".arrow").remove();
+    svg_places.selectAll(".places_text").remove();
 }
