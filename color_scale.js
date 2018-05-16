d47ed9272dac3d87b446dc992590f5da6e6c92ed
fsbolgi <francesca.sbolgi@gmail.com>
2018-05-16 19:24:51 +0200

    script to create color for timeline

---
+++ color_scale.js
@@ -0,0 +1,23 @@
+function time_color_scales () {
+    var past_scale = d3.scale.linear().domain([1,30])
+        .interpolate(d3.interpolateHcl)
+        .range([d3.rgb("#ffcc00"), d3.rgb('#b30000')]);
+    var present_scale = d3.scale.linear().domain([1,35])
+        .interpolate(d3.interpolateHcl)
+        .range([d3.rgb("#99ff33"), d3.rgb('#006600')]);
+    var future_scale = d3.scale.linear().domain([1,48])
+        .interpolate(d3.interpolateHcl)
+        .range([d3.rgb("#99ccff"), d3.rgb('#000099')]);
+
+    var color_scale = new Array(114);
+    for (i = 0; i<31; i++){
+        color_scale[i] = past_scale(i);
+    }
+    for (i = 0; i<36; i++){
+        color_scale[i+30] = present_scale(i);
+    }
+    for (i = 0; i<49; i++){
+        color_scale[i+66] = future_scale(i);
+    }
+    return color_scale;
+}
