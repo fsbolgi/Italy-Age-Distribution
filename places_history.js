3bc7391d2fb6fca20262595bbd51894bb1c75cd0
fsbolgi <francesca.sbolgi@gmail.com>
2018-05-25 01:13:36 +0200

    change layout

--- places_history.js
+++ places_history.js
@@ -1,55 +1,33 @@
 var svg_places = d3.select(".places_history_svg"), // select correct svg
-    x = 90,
-    y = 10;
-var grp = svg_places.append("g")
-    .attr("transform", "translate(" + x + "," + y + ")");
+    x = 40,
+    y = 40;
 
-var ita = grp.append("text")
+var place = svg_places.append("text")
     .text("ITALIA")
-    .style("fill", "#f7f6ee")
-    .attr("text-anchor", "middle")
-    .attr("dy", ".35em")
+    .attr("dx", x)
+    .attr("dy", y)
     .attr("class", "places_text_IT");
 
-ita.transition().duration(1000).style("fill", "#696630");
+place.transition().duration(1000).style("opacity", 1);
+
+draw_magnifier();
 
 function draw_arrow(el_name, level) {
-    var line = svg_places.append("line") // draw line
-        .attr("x1", x)
-        .attr("y1", (y + 25 + 180 * level))
-        .attr("x2", x)
-        .attr("y2", (y + 150 + 180 * level))
-        .attr("stroke-width", 0)
-        .attr("stroke", "#696630")
-        .transition()
-        .duration(700)
-        .attr("stroke-width", 1.7)
-        .attr("marker-end", "url(#arrow)")
-        .attr("class", "arrow "+ level);
-
-    grp = svg_places.append("g")
-        .attr("transform", "translate(" + x + "," + (y + 180 * (level + 1)) + ")");
-
     var min_font = 0;
 
-    svg_places.selectAll("text").on("click", function () { // make previous levels
-    });
-
-    var place_name = grp.append("text")
-        .text(extract_properties(el_name)[0].toUpperCase())
-        .attr("text-anchor", "middle")
-        .attr("dy", ".35em")
+    place = svg_places.append("text")
+        .text("> " +extract_properties(el_name)[0].toUpperCase())
+        .attr("dx", x)
+        .attr("dy", (y + 40 * (level + 1)))
         .attr("class", "places_text "+level)
-        .style("font-size", 1)
-        .on("click", function () {
-            zoom_out();
-        });
-    place_name.transition()
-        .duration(700)
+        .style("font-size", 1);
+    place.transition()
+        .duration(1000)
         .style("font-size", function (d) {
-            min_font = Math.min(16, (200) / this.getComputedTextLength() * 0.55);
+            min_font = Math.min(14, (200) / this.getComputedTextLength() * 0.55);
             return min_font + "pt";
-        });
+        })
+        .style("opacity", 1);
 }
 
 function remove_level() {
