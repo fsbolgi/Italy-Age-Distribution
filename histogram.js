366f4af15edffcb7271174518b9a2e8bc3271fa5
fsbolgi <francesca.sbolgi@gmail.com>
2018-06-21 17:07:01 +0200

    fixed bugs

--- histogram.js
+++ histogram.js
@@ -4,48 +4,11 @@ var width = 600,
     svg_histoB = d3.select(".histoB_svg"),
     start = 2,
     ages_array = new Array(100),
-    year_modification = false;
+    year_modification = false,
+    group_by = 1,
+    tooltip_histo;
 
-for (i = 0; i <= 100; i++) {
-    ages_array[i] = i;
-}
-
-var ages_name = svg_histoB.selectAll("text")
-    .data(ages_array)
-    .enter()
-    .append("text")
-    .text(function (d) {
-        if (d % 10 == 0) {
-            return d;
-        } else {
-            return "";
-        }
-    })
-    .attr("dx", function (d) {
-        if (d == 0) {
-            return 10;
-        } else if (d == 100) {
-            return 4;
-        }else {
-            return 7;
-        }
-    })
-    .attr("dy", function (d, i) {
-        return 5.3 * i + 17;
-    })
-    .attr("class", "year_label");
-ages_name.transition()
-    .duration(0)
-    .delay(function (d, i) {
-        return 1000 + i * 9;
-    })
-    .style("opacity", 1);
-
-var tooltip = d3.select("body")
-    .append("div")
-    .attr("class", "tooltip")
-    .style("position", "absolute")
-    .style("visibility", "hidden");
+draw_histo_label();
 
 var col_name = "A_1982",
     file_nameA = "Data/Male/ITALIA.csv",
@@ -62,90 +25,89 @@ function draw_histo(path_data, svg_histo, pos) {
 
     d3.csv(path_data, type, function (error, data) {
 
+        var data_grouped = compute_grouping(data);
+
         var tot_pop = d3.sum(data, function (d) {
             return d.value;
         });
 
-        curr_max = 0;
-        var save_col_name = col_name;
-        var max = 0;
-
-        var cols = d3.keys(data[0]);
-
-        for (i = 1; i < cols.length - 1; i++) {
-            col_name = cols[i];
-            data.forEach(function (d) {
-                d.col = +d[col_name];
-            });
-
-            max = d3.max(data, function (d) {
-                return d.col;
-            });
-            if (max > curr_max) {
-                curr_max = max;
-            }
-        }
-        col_name = save_col_name;
+        var curr_max = compute_max(data);
 
         var xScale = d3.scale.linear()
-            .domain([0, curr_max])
+            .domain([0, curr_max * group_by])
             .range([0, width / 2 - 10]);
 
-        var bar_height = (height-30) / ages_array.length;
+        var bar_height = (height - 30) * group_by / ages_array.length;
+
+        var mean = compute_mean_age(data, tot_pop);
 
         var rect = svg_histo.selectAll("rect")
-            .data(data);
+            .data(data_grouped);
 
-        rect.enter()
+        rect.enter() // insert histogram bars
             .append("rect")
             .attr("width", 0)
-            .attr("height", bar_height)
-            .attr("x", function (d) {
+            .attr("x", function () {
                 if (pos == "left") {
                     return 275;
                 } else {
                     return 40;
                 }
-            })
-            .attr("y", function (d, i) {
-                return i * (height-20) / data.length + 10;
             });
 
-        rect.transition()
-            .delay(function () {
-                return (start > 0) ? 800 : 0;
+        rect.transition() // transition on enter or on update
+            .delay(function (d, i) {
+                return (start > 0) ? (800 + i*7) : 0;
             })
             .duration(function () {
                 return year_modification ? 100 : 1000;
             })
-            .attr("width", function (d) {
-                return xScale(d.value);
+            .style("fill", function (d, i) {
+                if (i == mean) {
+                    return (pos == "left") ? "#265073" : "#f98699";
+                }
             })
+            .attr("width", function (d) {
+                return xScale(d);
+            })
+            .attr("height", bar_height)
             .attr("x", function (d) {
                 if (pos == "left") {
-                    return (width / 2 - (xScale(d.value)));
+                    return (width / 2 - (xScale(d)));
                 } else {
                     return 25;
                 }
+            })
+            .attr("y", function (d, i) {
+                return i * (height - 20) / data_grouped.length + 10;
             });
+
+        rect.exit().transition().duration(150).remove(); // transition on exit
+
         if (!year_modification) {
             start--;
         }
 
         rect.on('mouseover', function () {
             d3.select(this).style("opacity", 1);
-            tooltip.style("visibility", "visible");
+            tooltip_histo.style("visibility", "visible");
         })
             .on('mouseout', function () {
                 d3.select(this).style("opacity", .7);
-                tooltip.style("visibility", "hidden");
+                tooltip_histo.style("visibility", "hidden");
             })
             .on("mousemove", function (d, i) {
-                tooltip.style("top", (event.pageY - 15) + "px")
-                    .style("left", (event.pageX + 17) + "px")
-                    .html("&nbsp<strong>Age:</strong> " + i + "<br>" +
-                        "&nbsp<strong>Value:</strong> " + d.value + "<br>" +
-                        "&nbsp<strong>Rate:</strong> " + Math.round((d.value * 10000 / tot_pop)) / 100 + "%");
+                tooltip_histo.style("top", (event.pageY - 15) + "px")
+                    .style("left", (event.pageX + 17) + "px");
+                var age_value = i;
+                if (group_by > 1) {
+                    age_value = Math.round(i * group_by) + " - " + Math.round(i * group_by + group_by - 1);
+                }
+                var tool_string = "&nbsp<strong>Age:</strong> " + age_value + "<br>" +
+                    "&nbsp<strong>Value:</strong> " + d + "<br>" +
+                    "&nbsp<strong>Rate:</strong> " + Math.round((d * 10000 / tot_pop)) / 100 + "%";
+                (i == mean) ? tool_string = "&nbsp<strong>AVERAGE AGE</strong><br>" + tool_string : 0;
+                tooltip_histo.html(tool_string);
             });
     });
 }
@@ -153,4 +115,79 @@ function draw_histo(path_data, svg_histo, pos) {
 function type(d) {
     d.value = +d[col_name];
     return d;
+}
+
+function compute_max(data) { // compute the max of the whole file
+    var curr_max = 0;
+    var save_col_name = col_name;
+    var max = 0;
+
+    var cols = d3.keys(data[0]);
+
+    for (i = 1; i < cols.length - 1; i++) {
+        col_name = cols[i];
+        data.forEach(function (d) {
+            d.col = +d[col_name];
+        });
+
+        max = d3.max(data, function (d) {
+            return d.col;
+        });
+        if (max > curr_max) {
+            curr_max = max;
+        }
+    }
+    col_name = save_col_name;
+    return curr_max;
+}
+
+function compute_grouping(data) {
+    var data_grouped = new Array(100 / group_by);
+    for (i = 0; i < (100 / group_by); i++) {
+        data_grouped[i] = 0;
+    }
+    var c = 0;
+    for (i = 0; i < 100; i++) {
+        data_grouped[c] = data_grouped[c] + data[i].value;
+        if ((i + 1) % group_by == 0) {
+            c++;
+        }
+    }
+    return data_grouped;
+}
+
+function compute_mean_age(data, tot_pop) {
+    var tot_pop_weighted = d3.sum(data, function (d, i) {
+        return d.value * (ages_array[i] + 1);
+    });
+    return Math.round((tot_pop_weighted / tot_pop - 1) / group_by);
+}
+
+function zoom_in_histo(curr_el) {
+    year_modification = false;
+    var el_name = extract_properties(curr_el)[0].toUpperCase();
+    if (level == 2) { // if there's a mun with the same name of a province
+        var url = "Data/Male/" + el_name + "_mun.csv";
+        var http = new XMLHttpRequest();
+        http.open('HEAD', url, false);
+        http.send();
+        if (http.status != 404) {
+            el_name = el_name + "_mun";
+        }
+    }
+    var url = "Data/Male/" + el_name + ".csv"; // check if
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    if (http.status != 404) {
+        file_nameA = "Data/Male/" + el_name + ".csv";
+        file_nameB = "Data/Female/" + el_name + ".csv";
+        draw_histo(file_nameA, svg_histoA, "left");
+        draw_histo(file_nameB, svg_histoB, "right");
+    } else {
+        svg_map.append("text")
+            .text("DATA NOT FOUND")
+            .attr("transform", "translate(" + 170 + "," + 20 + ")")
+            .attr("class", "data_not_found");
+    }
 }
--- timeline.js
+++ timeline.js
@@ -1,99 +1,26 @@
-var svg_time = d3.select(".time_svg"); // select correct svg
-var points = [[35, 30], [582, 10]];
-var line_width = 550;
-var n_ages = 114;
-var ages_array = new Array(114);
-var curr_year = 1952;
+var svg_time = d3.select(".time_svg"), // select correct svg
+    points = [[35, 30], [582, 30]],
+    line_width = 550,
+    n_ages = 114,
+    ages_array = new Array(114),
+    curr_year = 1952,
+    time_line,
+    past,
+    future,
+    years_name,
+    div_cursor,
+    text_cursor;
 
 for (i = 0; i <= n_ages; i++) {
-    ages_array[i] = curr_year;
-    curr_year++;
+    ages_array[i] = curr_year + i;
 }
 
 color_scale = time_color_scales();
 
-var time_line = svg_time.selectAll("rect")
-    .data(ages_array)
-    .enter()
-    .append("rect")
-    .attr("width", line_width / n_ages - 1)
-    .attr("height", 15)
-    .attr("x", function (d, i) {
-        return points[0][0] + line_width / n_ages * i;
-    })
-    .attr("y", points[0][1] + 2)
-    .attr("id", function (d) {
-        return "rect_time_" + d;
-    })
-    .style("fill", "#f7f6ee");
-time_line.transition()
-    .duration(0)
-    .delay(function (d, i) {
-        return 1000 + i * 8;
-    })
-    .style("fill", function (d, i) {
-        if (d < 1982) {
-            return color_scale[i];
-        } else if (d < 2017) {
-            return color_scale[i];
-        } else {
-            return color_scale[i];
-        }
-    });
-
-var past = svg_time.append("text")
-    .text("PAST")
-    .attr("dx", 90)
-    .attr("dy", 10)
-    .attr("class", "epoc_label")
-    .attr("fill", "orange");
-past.transition()
-    .delay(800)
-    .duration(1000)
-    .style("opacity", 1);
-svg_time.append("text")
-    .text("PRESENT")
-    .attr("dx", 230)
-    .attr("dy", 10)
-    .attr("class", "epoc_label")
-    .attr("fill", "green")
-    .transition()
-    .delay(1000)
-    .duration(1000)
-    .style("opacity", 1);
-var future = svg_time.append("text")
-    .text("FUTURE")
-    .attr("dx", 440)
-    .attr("dy", 10)
-    .attr("class", "epoc_label")
-    .attr("fill", "blue");
-future.transition()
-    .delay(1200)
-    .duration(1000)
-    .style("opacity", 1);
-
-var years_name = svg_time.selectAll("text")
-    .data(ages_array)
-    .enter()
-    .append("text")
-    .text(function (d) {
-        if (d % 10 == 0) {
-            return d;
-        } else {
-            return "";
-        }
-    })
-    .attr("dx", function (d, i) {
-        return points[0][0] - 9 + line_width / n_ages * i;
-    })
-    .attr("dy", 60)
-    .attr("class", "year_label");
-years_name.transition()
-    .duration(0)
-    .delay(function (d, i) {
-        return 1000 + i * 9;
-    })
-    .style("opacity", 1);
+draw_years_rect();
+draw_epoc();
+draw_years_label();
+draw_cursor();
 
 var path = svg_time.append("path") // create path along the line
     .datum(points)
@@ -102,16 +29,20 @@ var path = svg_time.append("path") // create path along the line
 var drag = d3.behavior.drag() // function for dragging circle
     .on("drag", dragged);
 
+div_cursor.call(drag)
+    .transition()
+    .delay(800)
+    .duration(1000)
+    .style("opacity", 1);
+
 function dragged() {
     var m = d3.mouse(svg_time.node()), // get mouse position in svg
         p = closestPoint(path.node(), m); // compute closest point in the path of the mouse position
 
-      year_modification = true;
-    if (level <= 1) {
-        var pos = Math.round((p[0] - points[0][0]) * n_ages / line_width) + 1952;
-    } else {
-        var pos = Math.round((p[0] - points[0][0]) * n_ages / line_width) + 1982;
-    }
+    year_modification = true;
+    var pos = Math.round((p[0] - points[0][0]) * n_ages / line_width);
+    (level <= 1) ? pos = pos + 1952 : pos = pos + 1982;
+
     div_cursor.style('left', p[0] - 16 + "px");
     text_cursor.text(pos);
     set_col_name("A_" + pos);
@@ -146,68 +77,65 @@ function distance(p1, p2) { // compute distance from point and mouse position
     return dx * dx + dy * dy;
 }
 
-var div_cursor = d3.select('.timeline').append('div')
-    .attr('class', "div_cursor")
-    .style("opacity", 0)
-    .style('position','absolute')
-    .style('top', 10 + "px")
-    .style('left', 163 + "px")
-    .style('width', 38 + "px")
-    .style('height', 40 + "px");
-
-var svg_cursor = div_cursor.append("svg")
-    .attr("width", 38 + "px")
-    .attr("height", 40 + "px");
-svg_cursor.append("rect")
-    .attr("transform", "translate(" + 15 + "," + 20 +")")
-    .attr("class", "cursor");
-
-var text_cursor = svg_cursor.append("text")
-    .text("1982")
-    .attr("transform", "translate(" + 3 + "," + 15 +")")
-    .style("fill", "#696630")
-    .style("opacity", 1);
-
-div_cursor.call(drag)
-    .transition()
-    .delay(800)
-    .duration(1000)
-    .style("opacity", 1);
-
-
-function disable_time_section() {
+function disable_time_section(disable) {
     time_line.transition()
         .duration(1000)
         .style("fill", function (d, i) {
-            if (d < 1982) {
-                return "#e0e0d1";
-            } else if (d < 2017) {
-                return color_scale[i];
+            if (d < 1982 || d > 2017) {
+                return disable ? "#e0e0d1" : color_scale[i];
             } else {
-                return "#e0e0d1";
+                return color_scale[i];
             }
         });
 
     past.transition()
         .duration(1000)
-        .style("fill", "#e0e0d1");
+        .style("fill", function () {
+            return disable ? "#e0e0d1" : "orange";
+        });
     future.transition()
         .duration(1000)
-        .style("fill", "#e0e0d1");
+        .style("fill", function () {
+            return disable ? "#e0e0d1" : "blue";
+        });
     years_name.transition()
         .duration(1000)
         .style("fill", function (d, i) {
-            if (d < 1982) {
-                return "#e0e0d1";
-            } else if (d < 2017) {
-                return color_scale[i];
+            if (d < 1982 || d > 2017) {
+                return disable ? "#e0e0d1" : 696630;
             } else {
-                return "#e0e0d1";
+                return color_scale[i];
             }
         });
 
-    points = [[180, 10], [342, 10]];
+    if (disable) {
+        points = [[180, 10], [348, 10]];
+    } else {
+        points = [[35, 10], [582, 10]];
+    }
+
     path = svg_time.append("path") // create path along the line
         .datum(points)
         .attr("d", d3.svg.line());
+
+    var pos = document.getElementById("div_cursor").style.left;
+    pos = pos.substring(0, pos.length - 2);
+    console.log(pos);
+    if (pos > 342) {
+        pos = 332;
+        text_cursor.text("2017");
+        set_col_name("A_2017");
+        draw_histo(file_nameA, svg_histoA, "left");
+        draw_histo(file_nameB, svg_histoB, "right");
+        console.log(pos);
+    } else if (pos < 180) {
+        pos = 162;
+        text_cursor.text("1982");
+        set_col_name("A_1982");
+        draw_histo(file_nameA, svg_histoA, "left");
+        draw_histo(file_nameB, svg_histoB, "right");
+        console.log(pos);
+    }
+
+    div_cursor.style('left', pos + "px");
 }
