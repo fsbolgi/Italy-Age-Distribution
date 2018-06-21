73c8f02b0bc5c3d4db5a0b36059744d5ae58253f
fsbolgi <francesca.sbolgi@gmail.com>
2018-06-21 17:08:43 +0200

    refactoring

--- info_tooltip.js
+++ info_tooltip.js
@@ -1,30 +1,24 @@
-function insert_info_tooltip (svg, info_text) {
-    var info_tool = d3.select("body")
-        .append("div")
-        .attr("class", "tooltip")
-        .style("position", "absolute")
-        .style("z-index", "10")
-        .style("visibility", "hidden");
+function insert_info_tooltip(svg) {
 
-    svg.append("text")
+    svg.append("text") // draw the icon
         .text("i")
         .attr("fill", "#f7f6ee")
-        .attr("dx", 28)
-        .attr("dy", 14)
+        .attr("dx", 82)
+        .attr("dy", 552)
         .attr("class", "info_i");
     svg.append("circle")
-        .attr("cx", 30)
-        .attr("cy", 10)
+        .attr("cx", 85)
+        .attr("cy", 545)
         .attr("class", "info_button")
-        .on('mouseover', function (d) {
-            info_tool.style("visibility", "visible");
-        })
-        .on('mouseout', function (d) {
-            info_tool.style("visibility", "hidden");
-        })
-        .on("mousemove", function (d, i) {
-            info_tool.style("top", (event.pageY - 15) + "px")
-                .style("left", (event.pageX + 17) + "px")
-                .html(info_text);
+        .on("click", function () {
+            on();
         });
+}
+
+function on() { // make overlay appear
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() { // remove overlay
+    document.getElementById("overlay").style.display = "none";
 }
