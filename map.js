75cc73911a2c4b13797362bf928be08860791581
fsbolgi <francesca.sbolgi@gmail.com>
2018-06-29 09:27:41 +0200

    inserted hover to show on histogram

--- map.js
+++ map.js
@@ -8,8 +8,11 @@ var size_svg = 550, // width = height of the map
     scale = size_svg * 5.2,
     translate = [size_svg / 2, size_svg / 1.2],
     previous_el = 0,
+    reg_el = 0,
+    scale_el = 0,
     first = true,
-    mun_selected;
+    do_it = false,
+    mun_selected = 0;
 
 var tooltip_map = d3.select("body")
     .append("div")
@@ -29,7 +32,9 @@ svg_map.append("rect")
     .attr("class", "background_map")
     .attr("width", size_svg)
     .attr("height", size_svg)
-    .on("click", zoom_out);
+    .on("click", function () {
+        zoom_out(level - 1);
+    });
 
 var g = svg_map.append("g");
 
@@ -63,7 +68,7 @@ function create_map(error, json_el) {
         first = false;
     }
 
-    if (level > 0) { //zoom and center the element selected
+    if (level > 0 || do_it) { //zoom and center the element selected
         if (level > 1) { // perform a multiple zoom if it's a province
             curr_el.style("stroke-width", 1.5 / scale + "px")
                 .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
@@ -77,11 +82,31 @@ function create_map(error, json_el) {
     }
     el_clickable = true;
 
-    curr_el.on("mouseover", function () {
+    curr_el.on("mouseover", function (d) {
         d3.select(this).style("opacity", 1);
         tooltip_map.style("visibility", "visible");
+        var place_name = extract_properties(d)[0].toUpperCase(); // show data on hover
+        year_modification = false;
+        if (mun_selected != 0) {
+            return;
+        }
+        if (level == 2) { // if there's a mun with the same name of a province
+            var url = "Data/Male/" + place_name + "_mun.csv";
+            var http = new XMLHttpRequest();
+            http.open('HEAD', url, false);
+            http.send();
+            if (http.status != 404) {
+                place_name = place_name + "_mun";
+            }
+        }
+        var file_nameA = "Data/Male/" + place_name + ".csv";
+        var file_nameB = "Data/Female/" + place_name + ".csv";
+        draw_hover_histo(file_nameA, svg_histoA, "left");
+        draw_hover_histo(file_nameB, svg_histoB, "right");
     })
         .on("mouseout", function () {
+            d3.selectAll(".rect_hover_histo_left").transition().duration(100).remove();
+            d3.selectAll(".rect_hover_histo_right").transition().duration(100).remove();
             if (mun_selected != this) {
                 d3.select(this).style("opacity", .7);
             }
@@ -101,25 +126,46 @@ function map_clicked(curr_el) {
         return;
     }
     el_clickable = false; // avoid multiple selection
-    write_next_place(curr_el, level); // call function in little_charts.js to insert arrow and region name
 
-    // update the histogram
-    zoom_in_histo(curr_el);
+    zoom_in_histo(curr_el);     // update the histogram
+    draw_tot_pop();
+    draw_n_births();
+    d3.selectAll(".rect_hover_histo_left").transition().duration(100).remove();
+    d3.selectAll(".rect_hover_histo_right").transition().duration(100).remove();
 
-    if (level >= 1) { // for prov and mun disable past and future
+    if (level == 1) { // for prov and mun disable past and future
         disable_time_section(true);
+        new_time_lenght = 2;
     }
 
     if (level == 2) {
+        if (mun_selected != 0){
+            d3.select(mun_selected).style("opacity", 0.7);
+            d3.select("#places_text_3")
+                .transition().duration(1200).style("opacity", 0).remove();
+            d3.select("#places_text_3").each(function (d) {
+                place_x = place_x - this.getComputedTextLength()-9;
+            });
+        }
+        write_next_place(curr_el, level); // call function in header to insert arrow and region name
         mun_selected = this;
         d3.select(this).style("opacity", 1);
+        el_clickable = true; // avoid multiple selection
         return;
     }
+
+    write_next_place(curr_el, level); // call function in header to insert arrow and region name
 
     g.selectAll("." + array_names[level]).transition() // shrink and then remove the other regions
         .duration(800)
         .style("opacity", 0)
         .remove();
+
+
+    if (level == 0) {
+        reg_el = curr_el;
+        scale_el = scale;
+    }
 
     previous_el = curr_el;
     el_CODE = extract_properties(curr_el)[1];
@@ -178,21 +224,40 @@ function translate_map() {
     return [size_svg / 2 - scale * box[2], size_svg / 2 - scale * box[3]];
 }
 
-function zoom_out() {
-    svgA_header.selectAll(".places_text").remove();
-    place_x = 80;
+function zoom_out(level_clicked) {
+    level_clicked = 0; ////////////////////////////////remove just for test
 
-    if (level == 0) {
+
+    if (level == 0 || level == level_clicked) {
         return;
     }
+    for (l = level_clicked + 1; l <= 3; l++) {
+        svgA_header.selectAll("#places_text_" + l)
+            .transition().duration(1200).style("opacity", 0).remove();
+        svgA_header.select("#places_text_" + l).each(function (d) {
+            place_x = place_x - this.getComputedTextLength();
+        });
+    }
+
+    place_x = place_x - 10;
+
+    var place_name;
+    svgA_header.selectAll(".places_text").filter(function (d, i) {
+        if (i === level_clicked) {
+            place_name = d3.select(this).text();
+            place_name = place_name.substring(place_name.indexOf(">")+1, place_name.length).trim();
+        }
+    });
 
     year_modification = false;
-    file_nameA = "Data/Male/ITALIA.csv";
-    file_nameB = "Data/Female/ITALIA.csv";
+    file_nameA = "Data/Male/" + place_name + ".csv";
+    file_nameB = "Data/Female/" + place_name + ".csv";
     draw_histo(file_nameA, svg_histoA, "left");
     draw_histo(file_nameB, svg_histoB, "right");
 
-    disable_time_section(false);
+    draw_n_births();
+    draw_tot_pop();
+    disable_time_section(false); ////////////change!
 
     svg_map.selectAll("text").remove();
 
@@ -200,19 +265,54 @@ function zoom_out() {
     if (level == 1) {
         offset = 4;
     } else {
+        new_time_lenght = 2;
         offset = 2;
     }
-    var prov_x = path_map.bounds(previous_el)[0][0] - offset,
-        prov_y = path_map.bounds(previous_el)[0][1] - offset,
-        box = compute_bounding_box(previous_el),
-        rect_diff = Math.abs(box[0] - box[1]) / 2;
-    (box[0] > box[1]) ? prov_y = prov_y - rect_diff : prov_x = prov_x - rect_diff;
 
-    g.transition()
-        .duration(1200)
-        .style("stroke-width", "1.5px")
-        .attr("transform", "translate(" + [prov_x, prov_y] + ")scale(" + 1 / scale + ")")
-        .each("end", next_level);
+    if (level_clicked == 0) { // zoom to italy
+        var prov_x = path_map.bounds(previous_el)[0][0] - offset,
+            prov_y = path_map.bounds(previous_el)[0][1] - offset,
+            box = compute_bounding_box(previous_el),
+            rect_diff = Math.abs(box[0] - box[1]) / 2;
+        (box[0] > box[1]) ? prov_y = prov_y - rect_diff : prov_x = prov_x - rect_diff;
+
+        g.transition()
+            .duration(1200)
+            .style("stroke-width", "1.5px")
+            .attr("transform", "translate(" + [prov_x, prov_y] + ")scale(" + 1 / scale  + ")")
+            .each("end", next_level);
+    } else { // zoom back to province ////////////////////////doesn't work!
+        var prov_x = path_map.bounds(previous_el)[0][0] - offset,
+            prov_y = path_map.bounds(previous_el)[0][1] - offset,
+            box = compute_bounding_box(previous_el),
+            rect_diff = Math.abs(box[0] - box[1]) / 2;
+        (box[0] > box[1]) ? prov_y = prov_y - rect_diff : prov_x = prov_x - rect_diff;
+
+        var reg_x = path_map.bounds(reg_el)[0][0] - offset,
+            reg_y = path_map.bounds(reg_el)[0][1] - offset,
+            box_reg = compute_bounding_box(reg_el),
+            rect_diff_reg = Math.abs(box_reg[0] - box_reg[1]) / 2;
+        (box_reg[0] > box_reg[1]) ? reg_y = reg_y - rect_diff_reg : reg_x = reg_x - rect_diff_reg;
+
+        console.log(scale);
+        console.log(scale_el);
+        console.log(Math.min(size_svg * 0.92 / box[0], size_svg * 0.92 / box[1]));
+        console.log(Math.min(size_svg * 0.92 / box_reg[0], size_svg * 0.92 / box_reg[1]));
+        var s = Math.min(size_svg * 0.92 / box_reg[0], size_svg * 0.92 / box_reg[1]);
+        g.transition()
+            .duration(1200)
+            .style("stroke-width", "1.5px")
+            .attr("transform", "translate(" + [Math.abs(prov_x-reg_x)*5.2, Math.abs(prov_y-reg_y)*5.2] + ")scale(" + 1 / s + ")")
+            .each("end", next_level);
+    }
+
+
+    /*svg_map.append("rect")
+        .attr("x", prov_x)
+        .attr("y", prov_y)
+        .attr("width", box[0])
+        .attr("height", box[1])
+        .style("opacity", "0.5");*/
 
     g.selectAll(".provinces")
         .transition()
@@ -225,8 +325,14 @@ function zoom_out() {
         .style("stroke-width", "0px")
         .remove();
 
+    mun_selected = 0;
     level = 0;
     el_CODE = 0;
+    //el_CODE = extract_properties(reg_el)[1];
+    //level = 1;
+    //previous_el = reg_el;
+    //do_it = true;
+    //scale = 1 / scale * 5.2;
     g = svg_map.append("g");
 }
 
